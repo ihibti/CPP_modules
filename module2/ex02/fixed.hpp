cd510a1528408aa6295483a8200d30003a8083ab
ismael hibti <ihibti@air-de-ismael.home>
2024-09-16 21:34:06 +0200

    oui

--- module2/ex02/fixed.hpp
+++ module2/ex02/fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: ihibti <ihibti@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 18:05:23 by ihibti            #+#    #+#             */
-/*   Updated: 2024/09/16 13:45:15 by ihibti           ###   ########.fr       */
+/*   Updated: 2024/09/16 13:55:01 by ihibti           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,12 @@ class Fixed
 	Fixed(const int raw);
 	Fixed(const float raw);
 	Fixed(const Fixed &other);
+	~Fixed();
 	int toInt(void)const;
 	float toFloat(void)const;
-	Fixed &operator=(const Fixed &other);
-	~Fixed();
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
+	Fixed &operator=(const Fixed &other);
 	friend std::ostream &operator<<(std::ostream &fd, const Fixed &fixed);
 };
 
