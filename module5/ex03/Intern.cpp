5a1bbbb0d8bd0c93012f88dd3143a7eec3d1608b
Ismael Hibti <ismaelhibti2000@gmail.com>
2024-10-14 12:38:13 +0200

    module 5 termine

--- module5/ex03/Intern.cpp
+++ module5/ex03/Intern.cpp
@@ -2,58 +2,59 @@
 #include <iostream>
 
 // Constructor
-Intern::Intern() {}
+Intern::Intern()
+{
+}
 
 // Copy constructor
-Intern::Intern(const Intern& other) {
-    *this = other;
+Intern::Intern(const Intern &other)
+{
+	*this = other;
 }
 
 // Assignment operator
-Intern& Intern::operator=(const Intern& other) {
-    (void)other;
-    return *this;
+Intern &Intern::operator=(const Intern &other)
+{
+	(void)other;
+	return (*this);
 }
 
-// Destructor
-Intern::~Intern() {}
-
-// Helper functions to create specific forms
-AForm* Intern::makeShrubbery(const std::string& target) const {
-    return new ShrubberyCreationForm(target);
+Intern::~Intern()
+{
 }
 
-AForm* Intern::makeRobotomy(const std::string& target) const {
-    return new RobotomyRequestForm(target);
+AForm *Intern::makeShrubbery(const std::string &target) const
+{
+	return (new ShrubberyCreationForm(target));
 }
 
-AForm* Intern::makePresidentialPardon(const std::string& target) const {
-    return new PresidentialPardonForm(target);
+AForm *Intern::makeRobotomy(const std::string &target) const
+{
+	return (new RobotomyRequestForm(target));
 }
 
-// makeForm method
-AForm* Intern::makeForm(const std::string& formName, const std::string& target) const {
-    typedef AForm* (Intern::*FormCreator)(const std::string& target) const;
+AForm *Intern::makePresidentialPardon(const std::string &target) const
+{
+	return (new PresidentialPardonForm(target));
+}
 
-    const std::string formNames[] = {
-        "shrubbery creation", 
-        "robotomy request", 
-        "presidential pardon"
-    };
+AForm *Intern::makeForm(const std::string &formName,
+	const std::string &target) const
+{
+	const std::string formNames[] = {"shrubbery creation", "robotomy request",
+		"presidential pardon"};
+	AForm *(Intern::*formCreators[])(const std::string &target) const = {&Intern::makeShrubbery,
+		&Intern::makeRobotomy, &Intern::makePresidentialPardon};
 
-    FormCreator formCreators[] = {
-        &Intern::makeShrubbery,
-        &Intern::makeRobotomy,
-        &Intern::makePresidentialPardon
-    };
+	for (int i = 0; i < 3; i++)
+	{
+		if (formName == formNames[i])
+		{
+			std::cout << "Intern creates " << formName << std::endl;
+			return (this->*formCreators[i])(target);
+		}
+	}
 
-    for (int i = 0; i < 3; i++) {
-        if (formName == formNames[i]) {
-            std::cout << "Intern creates " << formName << std::endl;
-            return (this->*formCreators[i])(target);
-        }
-    }
-
-    std::cerr << "Error: Form name '" << formName << "' does not exist." << std::endl;
-    return 0;
+	std::cerr << "Error: Form name '" << formName << "' does not exist." << std::endl;
+	return (0);
 }
