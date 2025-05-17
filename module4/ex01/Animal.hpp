bee856f91e95ff1c1661e5575ccb2d57f08f5955
Ismael Hibti <ismael.hibti2000@gmail.com>
2025-05-17 11:41:55 +0200

    same but for ex01

--- module4/ex01/Animal.hpp
+++ module4/ex01/Animal.hpp
@@ -21,30 +21,8 @@ class Animal
 	std::string type;
 };
 
-class Dog : public Animal
-{
-  public:
-	Dog();
-	Dog(const Dog &other);
-	Dog &operator=(const Dog &other);
-	virtual ~Dog();
-	virtual void makeSound() const;
 
-  private:
-	Brain *brain;
-};
 
-class Cat : public Animal
-{
-  public:
-	Cat();
-	Cat(const Cat &other);
-	Cat &operator=(const Cat &other);
-	virtual ~Cat();
-	virtual void makeSound() const;
 
-  private:
-	Brain *brain;
-};
 
 #endif
---
+++ module4/ex01/Cat.cpp
@@ -0,0 +1,35 @@
+#include"Cat.hpp"
+
+Cat::Cat() : Animal("cat"), brain(new Brain())
+{
+	std::cout << "Cat created\n";
+}
+
+Cat::Cat(const Cat &other) : Animal(other)
+{
+	brain = new Brain(*other.brain);
+	std::cout << "Cat copied\n";
+}
+
+Cat &Cat::operator=(const Cat &other)
+{
+	if (this != &other)
+	{
+		Animal::operator=(other);
+		*brain = *other.brain;
+	}
+	std::cout << "Cat assigned\n";
+	return (*this);
+}
+
+Cat::~Cat()
+{
+	delete	brain;
+
+	std::cout << "Cat destroyed\n";
+}
+
+void Cat::makeSound() const
+{
+	std::cout << "Meow!\n";
+}
