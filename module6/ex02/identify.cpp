abe1c46ad062aa5fb4ba53f4f01e2a6fc7fc23e2
Ismael Hibti <ismaelhibti2000@gmail.com>
2025-05-20 18:54:09 +0200

    changement sur le try catch block ex02

--- module6/ex02/identify.cpp
+++ module6/ex02/identify.cpp
@@ -29,12 +29,28 @@ void	identify(Base *p)
 // pareil mais avec une reference
 void	identify(Base &p)
 {
-	if (dynamic_cast<A *>(&p))
-		std::cout << "A" << std::endl;
-	else if (dynamic_cast<B *>(&p))
-		std::cout << "B" << std::endl;
-	else if (dynamic_cast<C *>(&p))
-		std::cout << "C" << std::endl;
-	else
-		std::cout << "Unknown type" << std::endl;
+    try 
+    {
+        (void)dynamic_cast<A &>(p);
+        std::cout << "A" << std::endl;
+        return;
+    }
+    catch (std::exception &e) {}
+    try 
+    {
+        (void)dynamic_cast<B &>(p);
+        std::cout << "B" << std::endl;
+        return;
+    }
+    catch (std::exception &e) {
+    }
+    try 
+    {
+        (void)dynamic_cast<C &>(p);
+        std::cout << "C" << std::endl;
+        return;
+    }
+    catch (std::exception &e) {
+    }
+    std::cout << "Unknown type" << std::endl;
 }
