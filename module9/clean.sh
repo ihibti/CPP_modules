8c05073eb50be218b844d557152b126bea230114
Ismael Hibti <ismaelhibti2000@gmail.com>
2025-05-28 13:26:10 +0200

    oui

--- module9/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Iterate over all subdirectories and run `make fclean` if a Makefile exists
-for dir in */; do
-    if [ -f "$dir/Makefile" ]; then
-        echo "Cleaning in $dir...";
-        make -C "$dir" fclean;
-    fi;
-done
